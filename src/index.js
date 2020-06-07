871f245ed942e5666721c1494fe1cbbc824325f1
adisher <adilsher973@gmail.com>
2020-06-07 17:33:19 +0500

    Update index.js
    Added Fragment and functions in five different ways.

--- src/index.js
+++ src/index.js
@@ -2,18 +2,43 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function Hi(props) {
+function Hi({plainFunc}) {
     return (
-        <div>
-            <strong>Hello from {props.name}</strong>
-            <h1>Adil</h1>
-            <h3>Adil</h3>
-            {10 + 10}
-        </div>
+        <React.Fragment>
+            <div>Hello from fragment! No need to use root div element anymore!!</div>
+            <div>Hello from {plainFunc}</div>    
+        </React.Fragment>
+                
     );
-
 }
 
-ReactDOM.render(<Hi name="index.js"/>, document.querySelector('#root'));
+const MyConst = function({constFunc}) {
+    return <div>Hello from {constFunc}</div>;
+}
+
+const ArrowConst = ({arrowFunc}) => {
+    return <div>Hello from {arrowFunc}</div>;
+}
+
+const ParensConst = ({parensFunc}) => (
+    <div>Hello from {parensFunc}</div>
+)
+
+const OneLineConst = ({oneLineFunc}) => <div>Hello from {oneLineFunc}</div>
+
+
+
+
+ReactDOM.render(
+    <>
+        <Hi plainFunc="plain function"/>
+        <MyConst constFunc="constant holding a function"/>
+        <ArrowConst arrowFunc="arrow function"/>
+        <ParensConst parensFunc="'without return keyword and replacing curly braces with parens function"/>
+        <OneLineConst oneLineFunc="short one line function without return keyword and parens"/>
+    </>,
+
+    document.querySelector('#root')
+    );
 
 
